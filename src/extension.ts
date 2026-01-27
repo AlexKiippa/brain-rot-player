8f4d0d9466dc77a89912c0977c8720d76d264ddd
Alex Kiippa <alex.kiippa@nitor.com>
2026-01-27 15:51:41 +0200

    Playable surfers game

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,13 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "brain-rot-player" is now active!');
 
 	const disposable = vscode.commands.registerCommand('brain-rot-player.brainrot', async () => {
-		const options = ['Subway Surfer', 'Minecraft parkour', 'Family Guy'];
+		const options = ['Subway Surfer', 'Minecraft parkour', 'Family Guy', 'Subway Surfers Game'];
 		const videoUrls: { [key: string]: string } = {
 			'Subway Surfer':
 				'https://d2h58dsjpbzmve.cloudfront.net/5csxi%2Ffile%2F94140f46bae185d7a6367a7c8fb8f19f_f1afdfc2695dd005086c509b7f01d75d.mp4?response-content-disposition=inline%3Bfilename%3D%2294140f46bae185d7a6367a7c8fb8f19f_f1afdfc2695dd005086c509b7f01d75d.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1769541073&Signature=IwnulSKE2pm3yx~xjqC37otrlEzCWyZMf9K8dvn2TJc5YieTZgi7yFxQabvc7GHgJkBj70QIac1v~PtyXcCRsVoEfNg9UOzOq6Uh~AKm~3x8EnmITNamvPuDxZ7OOSpafnCqLW27ZxI7geecn2TJhxaEyjQHIq8TU4U8--4oUGzq9RFQAzDWXLvA9-r9spYlM1hWERTdnMUzZND3jkX3LhC1eoqx9zSqisB1IxUUIr9uE-cor-vWizg0FHJWoW8jZdyXRyeazzAnlKnL~C1ODpmbNFQEdjtEiTjEUt~w0iPqQ~gANYwGONqphC3qYZH1cCWmqEk-c7RV2sTwBNyPVw__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ',
 			'Minecraft parkour': 'REPLACE_WITH_URL',
 			'Family Guy': 'REPLACE_WITH_URL',
+			'Subway Surfers Game': 'https://ly4fsf.csb.app/',
 		};
 
 		const selected = await vscode.window.showQuickPick(options, {
@@ -39,7 +40,14 @@ export function activate(context: vscode.ExtensionContext) {
 			const playerHtmlPath = path.join(context.extensionPath, 'src', 'player.html');
 			let playerHtml = fs.readFileSync(playerHtmlPath, 'utf8');
 
-			playerHtml = playerHtml.replace('{{VIDEO_URL}}', videoUrl);
+			let playerContent = '';
+			if (selected === 'Subway Surfers Game') {
+				playerContent = `<iframe id="game-iframe" src="${videoUrl}" style="border:none;" allowfullscreen></iframe>`;
+			} else {
+				playerContent = `<video id="video-player" src="${videoUrl}" autoplay muted controls playsinline preload="auto"></video>`;
+			}
+
+			playerHtml = playerHtml.replace('{{PLAYER}}', playerContent);
 			playerHtml = playerHtml.replace(/{{CSP_SOURCE}}/g, panel.webview.cspSource);
 
 			panel.webview.html = playerHtml;
